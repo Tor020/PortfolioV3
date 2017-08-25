6c6d82aa591b5ef524a7fe28b6036e03f0ffa75c
Kevin <Tor000020@gmail.com>
2017-08-24 23:44:23 -0400

    c
    c

---
+++ scripts/main.js
@@ -0,0 +1,27 @@
+
+// document.cookie = 
+// "theme=" + encondeURIComponent("green theme") +
+// "; max-age=" + 60*10 + 
+// "; path=/; domain="
+
+
+var styleswap = function(url, evt){
+
+  themeSS.setAttribute("href", url);
+
+}
+var themeSS = document.getElementById('theme');
+
+
+
+var journal = 'https://jenil.github.io/bulmaswatch/journal/bulmaswatch.min.css';
+var litera = 'https://jenil.github.io/bulmaswatch/litera/bulmaswatch.min.css';
+var lumen = 'https://jenil.github.io/bulmaswatch/lumen/bulmaswatch.min.css';
+var lux = 'https://jenil.github.io/bulmaswatch/lux/bulmaswatch.min.css';
+var sandstone = 'https://jenil.github.io/bulmaswatch/sandstone/bulmaswatch.min.css';
+var spacelab = 'https://jenil.github.io/bulmaswatch/spacelab/bulmaswatch.min.css';
+var cyborg = 'https://jenil.github.io/bulmaswatch/cyborg/bulmaswatch.min.css';
+var darkly = 'https://jenil.github.io/bulmaswatch/darkly/bulmaswatch.min.css';
+var flatly = 'https://jenil.github.io/bulmaswatch/flatly/bulmaswatch.min.css';
+var slate = 'https://jenil.github.io/bulmaswatch/slate/bulmaswatch.min.css' ;
+
