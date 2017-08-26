c18a16a1e9c03f8b29bb08c01afa584549d42e15
Kevin <Tor000020@gmail.com>
2017-08-26 03:42:21 -0400

    cc
    c

--- scripts/concatscripts.js
+++ scripts/concatscripts.js
@@ -1,25 +1,25 @@
 window.onload =function(){
   
-  //creates element
-  var newElement = document.createElement('span');
-  newElement.textContent='fucking stupid';
-  console.log(newElement);
+//   //creates element
+//   var newElement = document.createElement('span');
+//   newElement.textContent='fucking stupid';
+//   console.log(newElement);
 
-  var replaceElement = document.createElement('span');
-  replaceElement.textContent = 'what'
-  replaceElement.id = 'what'
+//   var replaceElement = document.createElement('span');
+//   replaceElement.textContent = 'what'
+//   replaceElement.id = 'what'
   
   
-  var foo = document.getElementById('landing-Fade-Container');
-  // foo.appendChild(newElement);
-  // foo.insertBefore(newElement, foo.firstElementChild);
-  // foo.removeChild(newElement);
-  foo.insertBefore(newElement, foo.firstElementChild);
-  foo.replaceChild(replaceElement, newElement)
+// id ='landing-Fade-Container-Child'
+//   var foo = document.getElementById('landing-Fade-Container');
+//   // foo.appendChild(newElement);
+//   // foo.insertBefore(newElement, foo.firstElementChild);
+//   // foo.removeChild(newElement);
+//   foo.insertBefore(newElement, foo.firstElementChild);
+//   foo.replaceChild(replaceElement, newElement)
 
-var parentcont = document.getElementById('landing-txt-container');
+// var parentcont = document.getElementById('landing-txt-container');
 
-console.log(parentcont.childNodes);
+// console.log(parentcont.childNodes);
 }
 
- 
